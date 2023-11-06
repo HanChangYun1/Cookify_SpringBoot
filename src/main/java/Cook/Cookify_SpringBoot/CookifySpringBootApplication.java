275523c11d8e4ad7bdd74bc01bf1c3188d897c30
qndrl1004 <qndrl1004@gmail.com>
2023-11-06 17:51:39 +0900

    백엔드 초기설정

---
+++ src/main/java/Cook/Cookify_SpringBoot/CookifySpringBootApplication.java
@@ -0,0 +1,13 @@
+package Cook.Cookify_SpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CookifySpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CookifySpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Cook/Cookify_SpringBoot/CookifySpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package Cook.Cookify_SpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CookifySpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
