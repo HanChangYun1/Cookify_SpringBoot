4c2a288def588f8d84b9efea84848b8d080e11fb
qndrl1004 <qndrl1004@gmail.com>
2023-11-29 14:07:07 +0900

    graphql remove

--- src/main/java/Cook/Cookify_SpringBoot/global/WebConfig.java
+++ src/main/java/Cook/Cookify_SpringBoot/global/WebConfig.java
@@ -13,8 +13,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:3000") // 프론트엔드 서버의 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // OPTIONS 메서드 추가
                 .allowedHeaders("Origin", "Content-Type", "Accept", "Authorization")
                 .allowCredentials(true);
+
+
     }
 }
