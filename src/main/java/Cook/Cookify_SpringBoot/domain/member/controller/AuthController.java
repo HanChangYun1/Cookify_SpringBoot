a91895cc964b04b69ba6b1096303640dd3c43b3c
qndrl1004 <qndrl1004@gmail.com>
2023-12-01 11:46:32 +0900

    repair login3

--- src/main/java/Cook/Cookify_SpringBoot/domain/member/controller/AuthController.java
+++ src/main/java/Cook/Cookify_SpringBoot/domain/member/controller/AuthController.java
@@ -1,6 +1,7 @@
 package Cook.Cookify_SpringBoot.domain.member.controller;
 
 import Cook.Cookify_SpringBoot.domain.member.entity.GoogleMember;
+import Cook.Cookify_SpringBoot.domain.member.service.CustomOAuth2UserService;
 import Cook.Cookify_SpringBoot.domain.member.security.SessionMember;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -15,28 +16,51 @@ import javax.servlet.http.HttpSession;
 public class AuthController {
 
     private final HttpSession httpSession;
+    private final CustomOAuth2UserService customOAuth2UserService;
 
     @Autowired
-    public AuthController(HttpSession httpSession) {
+    public AuthController(HttpSession httpSession, CustomOAuth2UserService customOAuth2UserService) {
         this.httpSession = httpSession;
+        this.customOAuth2UserService = customOAuth2UserService;
     }
 
     @GetMapping("/user")
-    public SessionMember getUser(@AuthenticationPrincipal GoogleMember user) {
-        return new SessionMember(user);
+    public ResponseEntity<SessionMember> getUser(@AuthenticationPrincipal GoogleMember user) {
+        if (user == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(new SessionMember(user));
     }
 
     @PostMapping("/login")
     public ResponseEntity<Void> login(@AuthenticationPrincipal GoogleMember user) {
-        // 로그인 로직을 수행하고 세션에 사용자 정보를 저장
-        httpSession.setAttribute("user", new SessionMember(user));
-        return ResponseEntity.ok().build();
+        try {
+            if (user == null) {
+                return ResponseEntity.badRequest().build();
+            }
+
+            // 로그인 로직을 수행하고 세션에 사용자 정보를 저장
+            httpSession.setAttribute("user", new SessionMember(user));
+
+            // Save or update user in the database using the service
+            customOAuth2UserService.saveOrUpdateUser(user);
+
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            // Handle exceptions appropriately (e.g., log them)
+            return ResponseEntity.status(500).build(); // Internal Server Error
+        }
     }
 
     @PostMapping("/logout")
     public ResponseEntity<Void> logout(HttpSession session) {
-        // 로그아웃 로직을 수행하고 세션에서 사용자 정보를 제거
-        session.invalidate();
-        return ResponseEntity.ok().build();
+        try {
+            // 로그아웃 로직을 수행하고 세션에서 사용자 정보를 제거
+            session.invalidate();
+            return ResponseEntity.ok().build();
+        } catch (Exception e) {
+            // Handle exceptions appropriately (e.g., log them)
+            return ResponseEntity.status(500).build(); // Internal Server Error
+        }
     }
 }
