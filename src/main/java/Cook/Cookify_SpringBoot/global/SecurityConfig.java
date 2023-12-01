48e1c5d71ec0fe7daf99b88e9560bd5d979ac155
qndrl1004 <qndrl1004@gmail.com>
2023-12-01 10:35:38 +0900

    repair login2

--- src/main/java/Cook/Cookify_SpringBoot/global/SecurityConfig.java
+++ src/main/java/Cook/Cookify_SpringBoot/global/SecurityConfig.java
@@ -1,27 +1,39 @@
 package Cook.Cookify_SpringBoot.global;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.header.HeaderWriter;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors().and()
-                // 다른 설정들 추가
+        http
+                .cors().and()
+                .headers()
+                .addHeaderWriter(new HeaderWriter() {
+                    @Override
+                    public void writeHeaders(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) {
+                        response.setHeader("Cross-Origin-Opener-Policy", "same-origin; disallow-popups");
+                    }
+                })
+                .and()
+                // Your existing configurations
                 .oauth2Login()
                 .successHandler((request, response, authentication) -> {
-                    // 로그인 성공 후의 동작 정의
-                    response.sendRedirect("http://localhost:3000"); // 리다이렉트할 URL
+                    // Define actions after successful login
+                    response.sendRedirect("http://localhost:3000"); // Redirect URL
                 })
                 .and()
                 .authorizeRequests()
-                .antMatchers("/oauth2/**", "/login/**", "/logout/**").permitAll() // 허용할 URL 패턴 추가
+                .antMatchers("/oauth2/**", "/login/**", "/logout/**","/start/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .csrf().disable(); // CSRF 비활성화 (테스트 환경에서만)
+                .csrf().disable(); // Disable CSRF (only for testing environment)
     }
 }
-
